273c069e5b7e06bf874cc8f4eb19444792bc35c0
goob30 <68452963+goob30@users.noreply.github.com>
2026-05-09 14:32:50 -0700

    adfsgsdfsdg

--- scenes/settings.gd
+++ scenes/settings.gd
@@ -6,9 +6,10 @@ var lastScene = ""
 var settingsDict = {}
 
 func _ready() -> void:
+	
 	serial = GdSerial.new()
-	$musicVolumeSlider.value = SettingsGlobal.settings.get("music_vol")
-	$sfxVolSlider.value = SettingsGlobal.settings.get("sfx_vol")
+	$Control/musicVolumeSlider.value = SettingsGlobal.settings.get("music_vol")
+	$Control/sfxVolSlider.value = SettingsGlobal.settings.get("sfx_vol")
 
 func _process(delta: float) -> void:
 	pass
@@ -21,22 +22,22 @@ func _on_scan_serial_button_pressed() -> void:
 			var port_info = ports[key]
 			com_ports.append(port_info["port_name"])
 		if com_ports.size() > 0:
-			$availabelComPortsLabel.text = "Ports: " + ", ".join(com_ports)
+			$Control/availabelComPortsLabel.text = "Ports: " + ", ".join(com_ports)
 		else:
-			$availabelComPortsLabel.text = "No COM ports found"
+			$Control/availabelComPortsLabel.text = "No COM ports found"
 
 
 func _on_back_button_pressed() -> void:
-	get_tree().change_scene_to_file(Global.last_scene_name)
+	$Control.visible = false
 
 
 func _on_music_volume_slider_drag_ended(value_changed: bool) -> void:
 	if value_changed:
-		SettingsGlobal.settings.set("music_vol", $musicVolumeSlider.value)
+		SettingsGlobal.settings.set("music_vol", $Control/musicVolumeSlider.value)
 		print(SettingsGlobal.settings.get("music_vol"))
 
 
 func _on_sfx_vol_slider_drag_ended(value_changed: bool) -> void:
 	if value_changed:
-		SettingsGlobal.settings.set("sfx_vol", $sfxVolSlider.value)
+		SettingsGlobal.settings.set("sfx_vol", $Control/sfxVolSlider.value)
 		print(SettingsGlobal.settings.get("sfx_vol"))
