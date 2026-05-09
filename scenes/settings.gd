0ca68146edf63ceb0e1a637083099d1dccff7a2f
goob30 <68452963+goob30@users.noreply.github.com>
2026-05-09 11:34:44 -0700

    settings menu stuff

--- scenes/settings.gd
+++ scenes/settings.gd
@@ -5,9 +5,23 @@ var serial: GdSerial
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	serial = GdSerial.new()
-	GdSerial.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_scan_serial_button_pressed() -> void:
+	if serial:
+		var ports = serial.list_ports()
+		var com_ports = []
+
+		for key in ports.keys():
+			var port_info = ports[key]
+			com_ports.append(port_info["port_name"])
+	
+		if com_ports.size() > 0:
+			$availabelComPortsLabel.text = "Ports: " + ", ".join(com_ports)
+		else:
+			$availabelComPortsLabel.text = "No COM ports found"
