73d21ab4a733c955e024ad71fb8cc3edaeed343f
goob30 <68452963+goob30@users.noreply.github.com>
2026-05-09 10:44:53 -0700

    changes to serial

--- root_test.gd
+++ root_test.gd
@@ -1,31 +1,38 @@
 extends Control
-# oindgsjik
+
 var serial: GdSerial
-var timer := 0.0
+var read_timer := 0.0
+var serial_line := ""
+var last_serial_line := ""
+const READ_INTERVAL = 1.0 / 30.0  # 30Hz
 
 func _ready():
 	serial = GdSerial.new()
 	print(serial.list_ports())
 	serial.set_port("COM5")
 	serial.set_baud_rate(115200)
-	if !serial.open():
+	if serial.open():
+		print("Serial opened")
+	else:
 		print("Failed to open port")
 
 func _process(delta):
-	timer += delta
-	if timer >= 1.0:
-		timer = 0.0
-		if serial.is_open():
-			serial.writeline("Ping")
-	var response = serial.readline()
-	if response != "":
-		print(response)
+	$Label.text = str(Engine.get_frames_per_second())
+	read_timer += delta
+	if read_timer >= READ_INTERVAL:
+		read_timer = 0.0
+		if serial and serial.is_open():
+			if serial.bytes_available() > 0:
+				serial_line = serial.readline().strip_edges()
+				if serial_line != last_serial_line and serial_line != "":
+					print("New data: ", serial_line)
+					last_serial_line = serial_line
+					# serial data logic handling
+
+func _on_button_pressed() -> void:
+	if serial and serial.is_open():
+		serial.writeline("led")
 
 func _exit_tree():
 	if serial and serial.is_open():
 		serial.close()
-
-
-func _on_button_pressed() -> void:
-	if serial.is_open():
-		serial.writeline("led")
