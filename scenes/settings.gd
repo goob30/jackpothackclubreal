3b26c32208a7575008afdbc498f93f5379a695f6
goob30 <68452963+goob30@users.noreply.github.com>
2026-05-09 11:19:39 -0700

    add settings

---
+++ scenes/settings.gd
@@ -0,0 +1,13 @@
+extends Control
+
+var serial: GdSerial
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	serial = GdSerial.new()
+	GdSerial.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
